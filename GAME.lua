2bcf53895d5d003fbaf1e5b15c8092acbc9b5b38
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-12-01 17:06:08 +0800

    QWQ

--- GAME.lua
+++
@@ -1,9 +0,0 @@
-function Game.Rule:OnPlayerConnect(player)
-
-end
-
-
-function Game.Rule:OnRoundStartFinished()
-	print(Game.Weapon.CreateAndDrop(Common.WEAPON.M134Minigun, {x=46,y=20,z=8}));
-end
-
--- 测试.lua
+++ 测试.lua
@@ -131,19 +131,22 @@ end
 		Class:Create(Box,"Box","Component");
 	end)();
 
-c1 = Class:New("Box",32,0);
+-- c1 = Class:New("Box",32,0);
 
-while c1~=nil do
-    print("table")
-    for key, value in pairs(c1) do
-        if type(value) == "number" then
-            print(key .. ":" .. value);
-        else
-            print(key);
-        end
-    end
-    c1 = getmetatable(c1);
+-- while c1~=nil do
+--     print("table")
+--     for key, value in pairs(c1) do
+--         if type(value) == "number" then
+--             print(key .. ":" .. value);
+--         else
+--             print(key);
+--         end
+--     end
+--     c1 = getmetatable(c1);
+-- end
+
+for key, value in pairs(_G) do
+    print(key);
 end
 
-
-
+print(_VERSION)
